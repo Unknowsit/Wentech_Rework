30eab6872c05dcfca188bb8e3c131d37e43aa36b
Unknowsit <markbanyapon2@gmail.com>
2025-09-11 04:21:02 +0700

    UI
    +Mainmenu_Update

--- Assets/UI/Delay.cs
+++ Assets/UI/Delay.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class Delay : MonoBehaviour
 {
-    public GameObject obj,obj2,obj3;
+    public GameObject obj,obj2,obj3,obj4;
 
     public void OnButtonClick()
     {
-        Invoke("MyFunction", 1f); 
+        Invoke("MyFunction", 1f);
+        Invoke("NewO", 3f);
     }
 
     void MyFunction()
@@ -14,5 +15,11 @@ public class Delay : MonoBehaviour
         obj.SetActive(true);
         obj2.SetActive(true);
         obj3.SetActive(true);
+        obj4.SetActive(false);
+    }
+
+    void NewO()
+    {
+        
     }
 }
