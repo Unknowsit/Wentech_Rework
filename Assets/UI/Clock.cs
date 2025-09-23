5126a904a9168233b2b45f79bef6dd18167cd8d2
Unknowsit <markbanyapon2@gmail.com>
2025-09-24 06:46:07 +0700

    UI
    +Fix_Gameplay
    +Intro

---
+++ Assets/UI/Clock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        float seconds = time.Second + time.Millisecond / 1000f;
+        float angle = -seconds * 6f; 
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+}
