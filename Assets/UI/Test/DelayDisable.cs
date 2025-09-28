c02c4c507951dcaa190f90d38b4e82fb54560834
Unknowsit <markbanyapon2@gmail.com>
2025-09-29 06:39:02 +0700

    UI
    +Test_Operator_Done

--- Assets/UI/Test/DelayDisable.cs
+++ Assets/UI/Test/DelayDisable.cs
@@ -6,9 +6,20 @@ public class DelayDisable : MonoBehaviour
     public GameObject Object_1;
     public GameObject Object_2;
     public GameObject Object_3;
+    public GameObject Object_32;
     public GameObject Object_4;
     public GameObject Object_5;
     public GameObject Object_6;
+    public GameObject Object_7;
+    public GameObject Object_8;
+    public GameObject Object_9;
+    public GameObject Object_10;
+    public GameObject Object_11;
+
+    public GameObject Object_12;
+    public GameObject Object_13;
+    public GameObject Object_14;
+    public GameObject Object_15;
 
     public void Delay1Right()
     {
@@ -24,7 +35,30 @@ public class DelayDisable : MonoBehaviour
         StartCoroutine(DelayAction(Object_2, true, 1f));
 
         // (ถ้าต้องการ) ปรับ order ของ Object_2 เป็น 0 ทันที คงไว้ 2 วิ
-        // StartCoroutine(DelayOrderAction(Object_2, 0, 2f, 0f));
+        StartCoroutine(DelayOrderAction(Object_2, 0, 2f, 0f));
+
+        yield break;
+    }
+
+
+    public void Delay4left()
+    {
+        StartCoroutine(DelayObject4_Left());
+    }
+
+    IEnumerator DelayObject4_Left()
+    {
+        // ปิด Object_1 หลัง 1.1 วิ
+        StartCoroutine(DelayAction(Object_32, false, 1.1f));
+
+        // เปิด Object_1 หลัง 1 วิ
+        StartCoroutine(DelayAction(Object_3, true, 1f));
+
+        // ✅ ให้ Object 4,5,6 หน่วง 1 วิ ก่อน แล้วค่อยเปลี่ยน order เป็น 0 คงไว้ 2 วิ
+        StartCoroutine(DelayOrderAction(Object_12, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_13, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_14, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_15, 0, 2f, 1f));
 
         yield break;
     }
@@ -46,6 +80,73 @@ public class DelayDisable : MonoBehaviour
         StartCoroutine(DelayOrderAction(Object_4, 0, 2f, 1f));
         StartCoroutine(DelayOrderAction(Object_5, 0, 2f, 1f));
         StartCoroutine(DelayOrderAction(Object_6, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_7, 0, 2f, 1f));
+
+        yield break;
+    }
+
+    public void Delay3Left()
+    {
+        StartCoroutine(DelayObject3_Left());
+    }
+
+    IEnumerator DelayObject3_Left()
+    {
+        // ปิด Object_2 หลัง 1.1 วิ
+        StartCoroutine(DelayAction(Object_3, false, 1.1f));
+
+        // เปิด Object_1 หลัง 1 วิ
+        StartCoroutine(DelayAction(Object_2, true, 1f));
+
+        // ✅ ให้ Object 4,5,6 หน่วง 1 วิ ก่อน แล้วค่อยเปลี่ยน order เป็น 0 คงไว้ 2 วิ
+        StartCoroutine(DelayOrderAction(Object_8, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_9, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_10, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_11, 0, 2f, 1f));
+
+        yield break;
+    }
+
+    public void Delay2Right()
+    {
+        StartCoroutine(DelayObject2_Right());
+    }
+
+    IEnumerator DelayObject2_Right()
+    {
+        // ปิด Object_2 หลัง 1.1 วิ
+        StartCoroutine(DelayAction(Object_2, false, 1.1f));
+
+        // เปิด Object_1 หลัง 1 วิ
+        StartCoroutine(DelayAction(Object_3, true, 1f));
+
+        // ✅ ให้ Object 4,5,6 หน่วง 1 วิ ก่อน แล้วค่อยเปลี่ยน order เป็น 0 คงไว้ 2 วิ
+        StartCoroutine(DelayOrderAction(Object_4, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_5, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_6, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_7, 0, 2f, 1f));
+
+        yield break;
+    }
+
+    public void Delay3Right()
+    {
+        StartCoroutine(DelayObject3_Right());
+    }
+
+    IEnumerator DelayObject3_Right()
+    {
+        // ปิด Object_2 หลัง 1.1 วิ
+        StartCoroutine(DelayAction(Object_3, false, 1.1f));
+
+        // เปิด Object_1 หลัง 1 วิ
+        StartCoroutine(DelayAction(Object_32, true, 1f));
+
+        // ✅ ให้ Object 4,5,6 หน่วง 1 วิ ก่อน แล้วค่อยเปลี่ยน order เป็น 0 คงไว้ 2 วิ
+        StartCoroutine(DelayOrderAction(Object_8, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_9, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_10, 0, 2f, 1f));
+        StartCoroutine(DelayOrderAction(Object_11, 0, 2f, 1f));
 
         yield break;
     }
