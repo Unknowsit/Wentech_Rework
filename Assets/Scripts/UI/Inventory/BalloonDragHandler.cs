493af58f0e14d044fcbcf0391c1d56918d7b72bc
Tavin <Tavin.ratt@bumail.net>
2025-10-15 02:21:53 +0700

    UpdateSFXInGameplay

--- Assets/Scripts/UI/Inventory/BalloonDragHandler.cs
+++ Assets/Scripts/UI/Inventory/BalloonDragHandler.cs
@@ -7,12 +7,19 @@ public class BalloonDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
     [HideInInspector] public Transform originalSlot;
     public Image balloonImage;
 
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = AudioManager.instance;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         originalSlot = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
         balloonImage.raycastTarget = false;
+        audioManager.PlaySFX("SFX02");
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -24,5 +31,6 @@ public class BalloonDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
     {
         transform.SetParent(originalSlot);
         balloonImage.raycastTarget = true;
+        audioManager.PlaySFX("SFX03");
     }
 }
