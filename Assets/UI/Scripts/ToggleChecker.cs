457af2bea61b89ee6154d65cda1fea80c899ae15
Unknowsit <markbanyapon2@gmail.com>
2025-11-21 17:48:36 +0700

    Tutorial

---
+++ Assets/UI/Scripts/ToggleChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleCheckerButton : MonoBehaviour
+{
+    public Toggle[] toggles;        // ใส่ Toggle ที่ต้องตรวจ
+    public GameObject targetObject; // Object ที่จะเปิด/ปิด
+
+    // เรียกฟังก์ชันนี้จาก Button OnClick()
+    public void CheckToggles()
+    {
+        int count = 0;
+
+        foreach (var t in toggles)
+        {
+            if (t.isOn) count++;
+        }
+
+        if (count >= 2)
+        {
+            targetObject.SetActive(true);
+        }
+    }
+}
